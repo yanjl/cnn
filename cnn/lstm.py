b1ce0ed61044ad87d9478598244089fc9bca72e2
yanjl <sinberyh@aliyun.com>
2017-09-19 08:20:46 +0800

    add unit test

--- cnn/lstm.py
+++ cnn/lstm.py
@@ -1,3 +1,13 @@
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D, Dropout, BatchNormalization
+from keras.initializers import he_normal
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+
+def add(a, b):
+    return a + b
+
+
 def main():
     pass
 
---
+++ cnn/tests/test_lstm.py
@@ -0,0 +1,5 @@
+from cnn.lstm import add
+
+
+def test_add():
+    assert add(2, 3) == 5
